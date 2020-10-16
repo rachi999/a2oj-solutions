921e93adbb8b507182497c193b6ab0cf44c85585
Krostic Gaming <brawlmonsters@gmail.com>
2020-10-16 16:52:27 +0530

    StringTask

--- StringTask.c
+++ StringTask.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	int i, l;
+	int i, len, x;
 	char word[100];
 	scanf("%s",word);
-	l=strlen(word);
-	for(i=0;i<l;i++){
+	len=strlen(word);
+	x=0;
+	for(i=0;i<len;i++){
 		if(word[i]>=65 && word[i]<=90){
 			word[i] = word[i]+32;
+			x=x+1;
 		}
 		if(!(word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'||word[i]=='y')){
 			printf(".%c",word[i]);
